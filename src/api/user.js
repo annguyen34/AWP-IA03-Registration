0ba58855138a494f9c135c6f531adfb0b253ea34
annguyen34 <21120508@student.hcmus.edu.vn>
2024-10-29 00:37:04 +0700

    update url

--- src/api/user.js
+++ src/api/user.js
@@ -1,37 +1,32 @@
-const API_URL = 'http://localhost:3000';
+import axios from 'axios';
+
+const API_URL = 'https://awp-ia-03-registration-be.vercel.app';
 
 // Function to handle user registration
 export async function registerUser(username, email, password) {
-  const response = await fetch(`${API_URL}/users/register`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ username, email, password }),
-  });
+  try {
+    const response = await axios.post(`${API_URL}/users/register`, {
+      username,
+      email,
+      password,
+    });
 
-  if (!response.ok) {
-    throw new Error(
-      'Failed to register user. Username or email already exists'
-    );
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to register user. Please try again');
   }
-
-  return response.json();
 }
 
 // Function to handle user login
 export async function loginUser(username, password) {
-  const response = await fetch(`${API_URL}/users/login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ username, password }),
-  });
+  try {
+    const response = await axios.post(`${API_URL}/users/login`, {
+      username,
+      password,
+    });
 
-  if (!response.ok) {
-    throw new Error('Failed to login user. Invalid username or password');
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to login user. Please try again');
   }
-
-  return response.json();
 }
